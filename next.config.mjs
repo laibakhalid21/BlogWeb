9992bed7fc95e454fa09938d0d0b8cc7dac7dd3b
coding-practice <laibakhalid2109@gmail.com>
2025-10-31 19:46:34 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
   },
   reactCompiler: true,
 };
